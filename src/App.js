39c162e00c6161622b8c3e820bf34098a970b27d
Kim Sangyeon <dho113@naver.com>
2020-01-15 18:06:35 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
 
-const Food = ({ favourite }) => {
-  return <h3>I love {favourite}</h3>;
+const Food = ({ name, picture }) => {
+  return (
+    <>
+      <h3>I love {name}</h3>
+      <img src={picture} />
+    </>
+  );
 };
+
+const dishes = [
+  {
+    name: 'kimchi',
+    picture: 'https://www.rotinrice.com/wp-content/uploads/2011/09/NapaCabbageKimchi-1.jpg',
+  },
+  {
+    name: 'ramen',
+    picture:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=1xw:0.9995002498750624xh;center,top&resize=480:*',
+  },
+  {
+    name: 'samgiopsal',
+    picture: 'https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG',
+  },
+  {
+    name: 'chukumi',
+    picture: 'http://www.yorivery.com/data/goods/16/10/43/1000000171/1000000171_detail_08.jpg',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO !!!</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="ramen" />
-      <Food favourite="samgiopsal" />
-      <Food favourite="chukumi" />
+      {dishes.map(({ name, picture }) => (
+        <Food name={name} picture={picture} />
+      ))}
     </div>
   );
 }
