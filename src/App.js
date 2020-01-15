3be550b0f22c6e6f64c0b623b4e7d0d60d5f8de3
Kim Sangyeon <dho113@naver.com>
2020-01-15 17:45:19 +0900

    #2.1 Reusable Component with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from './Potato';
+
+const Food = ({ favourite }) => {
+  return <h3>I love {favourite}</h3>;
+};
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO !!!</h1>
-      <Potato />
+      <Food favourite="kimchi" />
+      <Food favourite="ramen" />
+      <Food favourite="samgiopsal" />
+      <Food favourite="chukumi" />
     </div>
   );
 }
